bef0000af503d798179ae5ae6155b08a73961e86
barros <matheusbarros1305@gmail.com>
2025-02-06 21:29:50 -0500

    Added better description in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import SocialMedia from "./SocialMedia";
 
 const Hero: React.FC = () => {
+
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-black text-gray-900 dark:text-white flex flex-col items-center justify-center px-4 md:px-6"> {/* Added responsive padding */}
       <motion.h1 
@@ -15,7 +16,7 @@ const Hero: React.FC = () => {
         Hi, I&apos;m Matheus Barros
       </motion.h1>
       <p className="text-lg md:text-xl text-center text-gray-700 dark:text-gray-400 mb-6"> {/* Added text-center and responsive font size */}
-        Software Engineer | Cloud Enthusiast
+        Software Engineer | Data Analyst
       </p>
       <div className="flex flex-col items-center gap-4 md:flex-row md:justify-center"> {/* Centered buttons and responsive layout */}
         <a href="#contact" className="inline-flex items-center border border-gray-300 dark:border-gray-600 justify-center select-none rounded-full disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 bg-white text-black hover:bg-gray-200 dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800 text-base h-12 px-6 font-semibold w-full md:w-auto"> {/* Added width adjustments for mobile */}
